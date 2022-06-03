9a1435b943d88a5d74997bce737925261f62946a
IngOrdaz <91166458+IngOrdaz@users.noreply.github.com>
2022-06-03 15:46:55 -0500

    Commit final todo servia, solo faltaba lo de internet

---
+++ app/src/main/java/com/nbd/getrequest/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.nbd.getrequest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.nbd.getrequest.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var queue:RequestQueue
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //en activity this en fragment context
+        queue=Volley.newRequestQueue(this )
+        getPokemonList(10)
+
+    }
+    fun getPokemonList(listAmount: Int){
+        val url="https://pokeapi.co/api/v2/pokemon/?limit=${listAmount}"
+
+        val jsonRequest= JsonObjectRequest(url , Response.Listener<JSONObject> {
+            response ->
+            Log.i("JSONRESPONSE", response.getJSONArray("results").toString())
+        },
+        Response.ErrorListener { error ->
+         Log.w("JSONRESPONSE", error.message as String)
+        })
+
+        queue.add(jsonRequest)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        queue.cancelAll("Stopped")
+    }
+}
